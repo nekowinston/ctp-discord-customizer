3deda036dff18d149a0a9aeb3e3c9d163f2cd207
winston <hey@winston.sh>
2022-07-02 20:21:56 +0200

    ci: change to vercel, remove GitHub actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		},
-    paths: {
-      base: '/ctp-discord-customizer'
-    }
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
 	},
 	preprocess: [
 		preprocess({
